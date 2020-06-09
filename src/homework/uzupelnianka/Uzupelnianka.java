eab0b152eb9d80129c2efc61c4fb8954cfd78138
ToFatToBat <kpapiernia@wp.pl>
2020-06-09 18:11:35 +0200

    Uzupelnianka

---
+++ src/homework/uzupelnianka/Uzupelnianka.java
@@ -0,0 +1,20 @@
+package homework.uzupelnianka;
+
+public class Uzupelnianka {
+/*
+    Klasa to matryca dla obiektow
+    Obiekt to twor posiadajacy klase.
+    Zmienna to czesc obiektu ktora odpowiada za jakas ceche obiektu.
+    Każda zmienna musi mieć określony TYP, np. int, albo String.
+    Jeżeli deklarację zmiennej poprzedzimy słowem final, to TEJ ZMIENNEJ NIE NADAMY NOWEJ/INNEJ WARTOSCI
+    W klasie mogą się znajdować ZMIENNE, METODY, SETTERY, GETTERY i CONSTRUKTORY
+            … są wywoływane przez operator O TAKIEJ SAMEJ NAZWIE JAK NAZWA KLASY i służą do tworzenia nowych obiektów.
+    Jeśli w klasie zdefiniowano chociaż jeden KONSTRUKTOR to kompilator nie wygeneruje dla nas KONSTRUKTORA domyślnego.
+            Zakładając, że istnieje klasa Car, to przy zapisie:
+    Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);
+    Car car2 = car1;​
+    car1 i car2 to dwie REFERENCJE wskazujące na ten sam OBIEKT. - TU NIE WIEM CZY TO NIE JEST TAK ZE DE FACTO OBA OBIEKTY POSIADAJA TA SAMA WARTOSC ALE NIE ODNOSZA SIE DO TEGO SAMEGO MIEJSCA W PAMIECI.
+
+
+ */
+}
